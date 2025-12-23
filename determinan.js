9ff32f0392765d0121e39df62e545af6d128d861
henryemily098 <71964325+henryemily098@users.noreply.github.com>
2025-12-23 08:09:43 +0700

    Update determinan.js

--- determinan.js
+++ determinan.js
@@ -3,21 +3,21 @@
  * @param {[]} matrix 
  * @param {number} n 
  */
-function iterative(matrix, n) {
+function iterative(matrix) {
     let left = 0, right = 0;
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < 3; i++) {
         let k = i, total = 1;
-        for (let j = 0; j < n; j++) {
-            if(k >= n) k = 0;
+        for (let j = 0; j < 3; j++) {
+            if(k >= 3) k = 0;
             total *= matrix[j][k];
             k++;
         }
         left += total;
     }
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < 3; i++) {
         let k = i, total = 1;
-        for (let j = 0; j < n; j++) {
-            if(k < 0) k = n-1;
+        for (let j = 0; j < 3; j++) {
+            if(k < 0) k = 2;
             total *= matrix[j][k];
             k--;
         }
@@ -46,10 +46,10 @@ function innerRecursive(matrix, n, column = 0, row = 0, step) {
  * @param {[]} matrix 
  * @param {number} n 
  */
-function recursive(matrix, n, row, step = null) {
+function recursive(matrix, row, step = null) {
     if(step != null) {
-        if(row === n-1) return innerRecursive(matrix, n, 0, row, step);
-        else return innerRecursive(matrix, n, 0, row, step) + recursive(matrix, n, row+1, step); 
+        if(row >= 2) return innerRecursive(matrix, 3, 0, row, step);
+        else return innerRecursive(matrix, 3, 0, row, step) + recursive(matrix, row+1, step); 
     }
-    else return recursive(matrix, n, 0, 1) - recursive(matrix, n, 0, -1);
+    else return recursive(matrix, 0, 1) - recursive(matrix, 0, -1);
 }
